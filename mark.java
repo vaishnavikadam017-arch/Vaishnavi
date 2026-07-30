11a243f44345bb8bf5151f5cecc9ad4cfe6c6597
root <root@localhost.localdomain>
2026-07-30 16:39:55 +0530

    Successfully Commit

---
+++ mark.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class mark
+{
+	public static void main(String args[])
+	{
+		int Score=80;
+		if(Score>=80)
+			System.out.println("Grade A");
+		else
+			System.out.println("Grade B");
+	}
+}
+
